f61cb7dc09ee1051d4ff31fb812d9d2f01891afc
epenaranda007 <eugenepenaranda007@gmail.com>
2017-02-20 20:51:28 -0800

    SongQueueView complete

--- client/views/SongQueueEntryView.js
+++ client/views/SongQueueEntryView.js
@@ -1,9 +1,21 @@
 // SongQueueEntryView.js - Defines a backbone view class for the song queue entries.
 var SongQueueEntryView = Backbone.View.extend({
-  // your code here!
+
+  template: _.template('<span class="artist"><%- artist %></span> \
+                          <span class="title">(<%- title %>)</span> '),
+
+
   inititialize: function() {
     this.on('change', function() {
 
     });
+  },
+
+
+  render: function() {
+    return this.$el.html(this.template(this.model.attributes));
   }
+
 });
+
+
--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -1,15 +1,22 @@
 // SongQueueView.js - Defines a backbone view class for the song queue.
 var SongQueueView = Backbone.View.extend({
 
+  template: _.template('<div class="songQueueView"></div>'),
+
+  
   initialize: function() {
-    // this.on('change add', function() {
-    //   //this.first().playFirst();
-    //   console.log('event triggered');
-    // }, this);
+
+    this.collection.on('add remove', function() {
+      console.log('change add remove triggered')
+      this.render();
+    }, this);
   },
 
-  render: function() {
-    return this.$el;
+  render: function() { 
+    this.collection.forEach(function(song) {
+      var songQueueEntryView = new SongQueueEntryView({'model': song});
+      this.$el.append(songQueueEntryView.render());
+    }, this);
   }
 
 });
